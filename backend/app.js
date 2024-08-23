b76f341e04f94bb33b4308f3b386a9648807baf1
Kushagra-88 <kushagraagarwal827@gmail.com>
2024-08-24 01:12:19 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -14,13 +14,7 @@ import { newsLetterCron } from "./automation/newsLetterCron.js";
 const app = express();
 config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
